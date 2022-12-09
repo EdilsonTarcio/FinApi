179e25245f06d15aa363d257911de186dd1743fa
Edilson Tarcio <tarcio.ecp@gmail.com>
2022-12-08 22:52:37 -0300

    Adicionando Statement by date

--- src/index.js
+++ src/index.js
@@ -77,4 +77,17 @@ app.post("/withdraw",verifyIfExistesAccountCPF, (request, response)=>{
 
   return response.status(201).send()
 });
+app.get("/statement/date", verifyIfExistesAccountCPF, (request, response) => { 
+  const {customer} = request;
+  const {date} = request.query;
+  
+  const dateFormat = new Date(date);
+
+  const statement = customer.statement.filter(
+    (statement)=> statement.created_at.toDateString() == new Date(dateFormat).toDateString()
+  );
+  
+  return response.json(statement);
+});
+
 app.listen(22222);
