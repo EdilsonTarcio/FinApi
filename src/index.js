6d467b31b30f1603cf20c4c947d2a48d874d2176
Edilson Tarcio <tarcio.ecp@gmail.com>
2022-12-15 21:58:58 -0300

    Adicionando validação de senha e recebendo cep

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ app.use(express.json());
 const customers = []; // vetor para armazenar temporariamente as contas 
 //Middleware
 function verifyIfExistesAccountCPF(request, response, next) {
-  const {cpf} = request.headers;
+  const {cpf, senha} = request.headers;
   const customer = customers.find((customer) => customer.cpf === cpf); //.find() retorna um objeto 
   if(!customer){
     return response.status(400).json({error: "Conta não localizada por este CPF!"})
+  }
+  if(senha != customer.senha){
+    return response.status(400).json({error: "Senha incorreta!"})
   }
   request.customer = customer; // passar o request customer para as rotas que passarem pelo Middleware
   return next();
@@ -25,7 +28,7 @@ function getBalance(statement){
   return balance
 }
 app.post("/account", (request, response) => {
-  const {cpf, name } = request.body;
+  const {cpf, name, senha, cep } = request.body;
   const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf); // 3 iguais mesmo valor e mesmo tipo 
   if(customerAlreadyExists){ // se encontrar um customer
     return response.status(400).json({error: "Usuario já existe!"})
@@ -33,19 +36,21 @@ app.post("/account", (request, response) => {
   customers.push({ 
     cpf,
     name,
+    senha,
+    cep,
     id: uuidv4(),
     statement: []
   });
 
   return response.status(201).send(); //sen() não retorna informação e sim um status
 })
-//app.use(verifyIfExistesAccountCPF); // todas as rotas a baixo irão passar por esse Middleware
-app.get("/statement", verifyIfExistesAccountCPF, (request, response) => { 
+app.use(verifyIfExistesAccountCPF); // todas as rotas a baixo irão passar por esse Middleware
+app.get("/statement", (request, response) => { 
   //se informar a Middleware direto na rota apenas essa ira passar pela Middleware
   const {customer} = request; // recebendo o customer da Middleware
   return response.json(customer.statement);
 });
-app.post("/deposit", verifyIfExistesAccountCPF, (request, response) => {
+app.post("/deposit", (request, response) => {
   const {description, amount} = request.body;
   const {customer} = request; 
   const statementeOperation = {
@@ -57,7 +62,7 @@ app.post("/deposit", verifyIfExistesAccountCPF, (request, response) => {
   customer.statement.push(statementeOperation);
   return response.status(201).send();
 });
-app.post("/withdraw",verifyIfExistesAccountCPF, (request, response)=>{
+app.post("/withdraw", (request, response)=>{
   const {amount} = request.body
   const {customer} = request
 
@@ -77,7 +82,7 @@ app.post("/withdraw",verifyIfExistesAccountCPF, (request, response)=>{
 
   return response.status(201).send()
 });
-app.get("/statement/date", verifyIfExistesAccountCPF, (request, response) => { 
+app.get("/statement/date", (request, response) => { 
   const {customer} = request;
   const {date} = request.query;
   
@@ -89,25 +94,25 @@ app.get("/statement/date", verifyIfExistesAccountCPF, (request, response) => {
   
   return response.json(statement);
 });
-app.put("/account", verifyIfExistesAccountCPF, (request, response)=>{
+app.put("/account", (request, response)=>{
   const {name} = request.body
   const {customer} = request
   customer.name = name //troca o nome do customer pelo recebido na request
 
   return response.status(201).send()
 });
-app.get("/account", verifyIfExistesAccountCPF, (request, response) =>{
+app.get("/account", (request, response) =>{
   const{customer} = request
   return response.json(customer)
 });
-app.delete("/account", verifyIfExistesAccountCPF, (request, response)=>{
+app.delete("/account", (request, response)=>{
   const {customer} = request
   //splice
   customers.splice(customer, 1) 
   // a função js espera dois parametros de onde vai iniciar e onde vai parar, nesse caso vai remover uma posição do vetor
   return response.status(200).json(customer)
 });
-app.get("/balance", verifyIfExistesAccountCPF, (request, response)=>{
+app.get("/balance", (request, response)=>{
   const {customer} = request
 
   const balance = getBalance(customer.statement)
